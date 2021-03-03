3233ee4c3fbf092f58a14159186bb3ab39f01fbc
Saber2756 <72886687+Saber2756@users.noreply.github.com>
2021-03-03 11:14:10 -1000

    Add files via upload

---
+++ PackmanGame-master 2/README.txt
@@ -0,0 +1,6 @@
+Website url: https://yardenez.github.io/Packman/
+
+Please notice that when choosing the keys in the settings page, you must first press on the key you wish to set and than another keyboard key to your choice. 
+This key will now be associated to the movement you have just set.
+
+Have fun!
---
+++ PackmanGame-master 2/SettingsPage.js
@@ -0,0 +1,92 @@
+    /* Present rangeSlider value dynamically*/
+    function showRangeSliderValue(val) {
+        $('.range-slider span').text(val);
+    }
+
+    function catchUserChoice(button){
+            button.addEventListener("keydown", function (e) {
+                $("#key"+button.value).text(e.code);
+            }, false);
+    }
+
+    function randomizeAll(){
+        //randomize number of balls
+        let randomNumOfBalls =50+ Math.floor(Math.random()*40);
+        $('.range-slider span').text(randomNumOfBalls);
+        $('.range-slider input[type=range]').val(randomNumOfBalls);
+        //randomize balls color
+        let firstColor=getRandomColor();
+        $('input[name=5ptColor]').val(firstColor);
+        let secondColor=getRandomColor();
+        while(firstColor === secondColor)
+            secondColor=getRandomColor();
+        $('input[name=15ptColor]').val(secondColor);
+        let thirdColor=getRandomColor();
+        while(thirdColor === firstColor || thirdColor === secondColor)
+            thirdColor=getRandomColor();
+        $('input[name=25ptColor]').val(thirdColor);
+        //randomize number of monsters
+        let $options = $('select[name=numMonsters]').find('option'),
+            random=Math.floor(Math.random() * $options.length)+1;
+        $('select[name=numMonsters]').val(random);
+        //randomize Game duration
+        $('input[name=gameTime]').val(Math.floor(Math.random()*60)+60);
+        //randomize Game keys
+        $('#keyUp').text("ArrowUp");
+        $('#keyDown').text("ArrowDown");
+        $('#keyRight').text("ArrowRight");
+        $('#keyLeft').text("ArrowLeft");
+    }
+
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    function activateGame(){
+        let loggedUser=$("#loggedInUser > b").text();
+        if(loggedUser==='Stranger') {
+            alert("You must log in before playing!");
+            return;
+        }
+        else if(!allSettingsWereSet()) {
+            alert("Not all settings were set. Please check the setting page again");
+            return;
+        }
+        let gameDuration= $('input[name=gameTime]').val();
+        if (gameDuration <60) {
+            alert("Please enter game duration above 60 seconds");
+            return;
+        }
+        else{
+            setColorsForUserKey();
+            Start();
+            zoom("80%");
+            toggle('Game');
+        }
+    }
+
+    function setColorsForUserKey(){
+        //5pt
+        var fiveColor=$('input[name=5ptColor]').val();
+        $('input[name=5PtKeyColor]').val(fiveColor);
+        //15pt
+        var fifteenColor=$('input[name=15ptColor]').val();
+        $('input[name=15PtKeyColor]').val(fifteenColor);
+        //25pt
+        var twentyFiveColor=$('input[name=25ptColor]').val();
+        $('input[name=25PtKeyColor]').val(twentyFiveColor);    }
+
+    function allSettingsWereSet(){
+        let keysSelected = $('#keyUp').text()!=='' &&  $('#keyDown').text()!==''  && $('#keyRight').text()!==''
+        && $('#keyLeft').text();
+        let gameDurationSet = $('input[name=gameTime]').val()!=='';
+        if(keysSelected && gameDurationSet)
+            return true;
+        else
+            return false;
+    }
